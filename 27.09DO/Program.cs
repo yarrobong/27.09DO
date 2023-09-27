f6f9fea02247a0905fb76ba84758c3d062622292
yarrobong <oneboardsholi@gmail.com>
2023-09-27 22:45:17 +0500

    Добавьте файлы проекта.

---
+++ 27.09DO/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace doo
+{
+
+    class reshenie
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Введите число: ");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n > 0)
+            {
+                int sqrtN = (int)Math.Sqrt(n);
+
+                if (sqrtN * sqrtN == n)
+                {
+                    Console.WriteLine("Это квадратное число");
+                }
+                else
+                {
+                    Console.WriteLine("Это неквадратное число");
+                }
+
+                string result = (sqrtN * sqrtN == n) ? "True" : "False";
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine("Введите положительное число");
+            }
+        }
+    }
+}
